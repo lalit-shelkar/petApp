61d3e0f3a4effaa7310c31b25bca0b6c515c9f62
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-06-24 13:32:21 +0530

    final commit

--- App.js
+++ App.js
@@ -2,16 +2,15 @@ const express = require("express");
 const route=require("./routes/petRouter");
 const app=express();
 const port=3000;
-//const data=require("../resources/petData");
-
+const data=require("./resources/petData");
 app.use(express.json());
 //app.use("/",route);
 app.get("/",(req,res)=>{
     res.send("hehhehhe");
 });
-// app.get("/petData",(req,res)=>{
-//     res.send(data);
-// });
+app.get("/petData",(req,res)=>{
+     res.send(data);
+ });
 
 app.listen(port,()=>{
     console.log("App is running")
