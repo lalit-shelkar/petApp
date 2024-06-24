5cfc9e69d6285ec9383660aa2134860c26a9d3ad
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-06-24 13:14:03 +0530

    creating default route

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ const port=3000;
 
 
 app.use(express.json());
-app.use("/api/v1",route);
+//app.use("/",route);
+app.get("/",(req,res)=>{
+    res.send("hehhehhe");
+});
 
 app.listen(port,()=>{
     console.log("App is running")
