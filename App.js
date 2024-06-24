dfa4fc9b1aa69b881d47db3563554c1b80248ace
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-06-24 13:20:17 +0530

    creating default route

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ const express = require("express");
 const route=require("./routes/petRouter");
 const app=express();
 const port=3000;
-
+const data=require("../resources/petData");
 
 app.use(express.json());
-app.use("/api/v1",route);
+//app.use("/",route);
+app.get("/",(req,res)=>{
+    res.send("hehhehhe");
+});
+app.get("/petData",(req,res)=>{
+    res.send(data);
+});
 
 app.listen(port,()=>{
     console.log("App is running")
