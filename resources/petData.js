1029f2eb17f8eb9416b07ea784c19ab01b79e481
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-06-30 16:04:00 +0530

    edited 3 no img link

--- resources/petData.js
+++ resources/petData.js
@@ -26,7 +26,7 @@ const data=  [
     "spe": "Veterinary Behavioral",
     "rating": "4.9",
     "dis": "1.5",
-    "img": "https://png.pngtree.com/png-vector/20240125/ourlarge/pngtree-smiling-vet-doctor-with-pet-png-image_11491130.png",
+    "img": "https://s3-alpha-sig.figma.com/img/1f0a/fba8/06c26034e5ca5b20347eb8791b7c3d26?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c6faSOBuut2lMZ1Z723dNkB2zObrPKubmJZHRWBHZkqDZB~MmafiS5KTROWoZuyhgi0r0V19O1IUwoaNrhsC08MmPijIaUsAA8AzYgpgjvNX3Jbd3x8BOtEsRdKX~bMuY~n9jrVAM0j0b07iYPX0LmBTfisjKpg~FR9wnxQbph1AqZsp6FyE6b8J-qMtxBbLiO5ckD~8-U5Lq8t6~jP1cz8xjwsJKSMi9QVTX87Fyec2cagYjbmejYGLBQokWJZcsQFJUPTanQfj6FVgmFR6qjEKB~Q5rY8AljWt8AIuuGxFHJ8NFAxCb6ziX3mUFns6qqYmYfppPM9ozoQXHFM1Hg__",
     "about":
         "Dr. Maria Nai is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "5 Years",
