04962578f90cc38134ad9770febc76f59031a281
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-06-30 15:57:15 +0530

    image links added

--- resources/petData.js
+++ resources/petData.js
@@ -4,7 +4,7 @@ const data=  [
     "spe": "Veterinary Behavioral",
     "rating": "4.8",
     "dis": "1.0",
-    "img": "assets/vetarnary/dr1.jpeg",
+    "img": "https://s3-alpha-sig.figma.com/img/1f0a/fba8/06c26034e5ca5b20347eb8791b7c3d26?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c6faSOBuut2lMZ1Z723dNkB2zObrPKubmJZHRWBHZkqDZB~MmafiS5KTROWoZuyhgi0r0V19O1IUwoaNrhsC08MmPijIaUsAA8AzYgpgjvNX3Jbd3x8BOtEsRdKX~bMuY~n9jrVAM0j0b07iYPX0LmBTfisjKpg~FR9wnxQbph1AqZsp6FyE6b8J-qMtxBbLiO5ckD~8-U5Lq8t6~jP1cz8xjwsJKSMi9QVTX87Fyec2cagYjbmejYGLBQokWJZcsQFJUPTanQfj6FVgmFR6qjEKB~Q5rY8AljWt8AIuuGxFHJ8NFAxCb6ziX3mUFns6qqYmYfppPM9ozoQXHFM1Hg__",
     "about":
         "Dr. Maria Naiis is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "11 Years",
@@ -15,7 +15,7 @@ const data=  [
     "spe": "Veterinary Surgery",
     "rating": "4.9",
     "dis": "1.5",
-    "img": "assets/vetarnary/dr2.jpeg",
+    "img": "https://s3-alpha-sig.figma.com/img/7fba/26ce/6b98aa74f39064fb4545b960abade7be?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mkKd~9ytOmcQpPD45EgpgRx2Q~FeIqHwBE2tG1sctHi52teqgBQUZONuEsft-4Fs1mM8cosCwOhHhihbalFhI~AiMYh6Q9DAQktr8cS~JzgGFlRHTClKZOiTL-4J-SBkMLS2~Ag9eBfpzQO8cjg1h3Z4WN40Xt7VY3hNs~VCPRGyobqpUZxQGoT7UjF-UypN6uDttBLjXln8bHMhjdfop6V0H~PvFUKGLQb0dFB7w9Jkjj8OY3C0y82WY9CHumGsLq2yZjPJAyh37P9NMhbj~Pa6tWzwJrmTIFaTvgHcA7-Jw5sd1BndRtmhGV1awIUau~G3Z6ESWNe06NGo5SGxuw__",
     "about":
         "Dr. Vernon Chwe is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "10 Years",
@@ -26,7 +26,7 @@ const data=  [
     "spe": "Veterinary Behavioral",
     "rating": "4.9",
     "dis": "1.5",
-    "img": "assets/vetarnary/dr3.jpeg",
+    "img": "https://png.pngtree.com/png-vector/20240125/ourlarge/pngtree-smiling-vet-doctor-with-pet-png-image_11491130.png",
     "about":
         "Dr. Maria Nai is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "5 Years",
@@ -37,7 +37,7 @@ const data=  [
     "spe": "Veterinary Surgery",
     "rating": "4.9",
     "dis": "1.5",
-    "img": "assets/vetarnary/dr2.jpeg",
+    "img": "https://w7.pngwing.com/pngs/349/587/png-transparent-the-beauty-pet-doctor-holds-the-dog-dog-doctor-lovely-the-dog.png",
     "about":
         "Dr. Maria Nai is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "5 Years",
@@ -48,7 +48,7 @@ const data=  [
     "spe": "Veterinary Behavioral",
     "rating": "4.9",
     "dis": "1.5",
-    "img": "assets/vetarnary/dr3.jpeg",
+    "img": "https://w7.pngwing.com/pngs/349/587/png-transparent-the-beauty-pet-doctor-holds-the-dog-dog-doctor-lovely-the-dog.png",
     "about":
         "Dr. Maria Nai is a highly experienced veterinarian with 11 years of dedicated practice, showcasing a pro...",
     "exp": "5 Years",
